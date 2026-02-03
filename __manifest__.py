b333199017b1bdfc11866e0ecb479ed511658de9
Michelaine Estefany Fingal <160836538+BlueMichelle@users.noreply.github.com>
2026-02-03 01:00:07 +0100

    revisión

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
 Descripción del módulo de la tarea 14 para la gestión de una guardería para empleados.
     """,
 
-    'author': "AllKeysHUB",
+    'author': "BlueMichelle",
     'website': "127.0.0.1:8069",
 
     # Categories can be used to filter modules in modules listing
